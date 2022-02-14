34fc9c6b57cf698bef3197560a4f6229c07d38f9
tomascarignano98 <tomascarignano98@gmail.com>
2022-02-14 12:10:03 -0300

    Created pages and routes.

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import styles from './Navbar.module.css';
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar} role="banner">
+      <ul role="navigation">
+        <li className={styles.title}>myMoney</li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/signup">Signup</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/pages/signup/Signup.js
@@ -0,0 +1,5 @@
+import styles from './Signup.module.css';
+
+export default function Singup() {
+  return <div>Singup</div>;
+}
