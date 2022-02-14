db84b36dcb8354b2b348f274c9c03e1a9c10356a
tomascarignano98 <tomascarignano98@gmail.com>
2022-02-14 12:50:54 -0300

    Login form.

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,5 +1,38 @@
+import { useState } from 'react';
 import styles from './Login.module.css';
 
 export default function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setEmail('');
+    setPassword('');
+    console.log({ email, password });
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className={styles['login-form']}>
+      <h1>Login</h1>
+      <label>
+        <span>email</span>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>password</span>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button className="btn">Login</button>
+    </form>
+  );
 }
