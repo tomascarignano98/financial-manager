92a7e8f50df4a9d4fb514df9a1b5b03ffa9795da
tomascarignano98 <tomascarignano98@gmail.com>
2022-02-21 08:52:15 -0300

    Listing transactions and Firestore Queries.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,18 @@
+import { useQuery } from '../../hooks/useQuery';
+import TransactionsList from './TransactionsList';
 import TransactionForm from './TransactionForm';
 import styles from './Home.module.css';
 
 export default function Home() {
+  const { documents, error } = useQuery('transactions');
+  // console.log(documents);
+
   return (
     <main className={styles.container}>
-      <section className={styles.content}>transactions list</section>
+      <section className={styles.content}>
+        {error && <p>{error}</p>}
+        {documents && <TransactionsList transactions={documents} />}
+      </section>
       <aside className={styles.sidebar}>
         <TransactionForm />
       </aside>
